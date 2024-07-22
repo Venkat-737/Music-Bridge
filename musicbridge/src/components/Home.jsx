47321251788395a39e5c1d9e2c7719cdafb3fa57
Venkat-737 <venkatsrisai670@gmail.com>
2024-07-22 15:01:37 +0530

    changed http

--- musicbridge/src/components/Home.jsx
+++ musicbridge/src/components/Home.jsx
@@ -26,7 +26,7 @@ function Home({ selectedOption }) {
     setErrorMessage('');
 
     try {
-      const response = await fetch('http://10.128.0.2:5000/download', {
+      const response = await fetch('http://13.232.135.112:5000/download', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
